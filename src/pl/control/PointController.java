ea7213d7da3ebd1c1a142cb7a3db663dd833364f
Mateusz Wygoda <mateusz.wygoda@gmail.com>
2020-04-21 13:39:02 +0200

    add point controller + extension of class Point

---
+++ src/pl/control/PointController.java
@@ -0,0 +1,4 @@
+package pl.control;
+
+public class PointController {
+}
--- src/pl/data/Point.java
+++ src/pl/data/Point.java
@@ -1,7 +1,31 @@
 package pl.data;
 
 public class Point {
-    private double x;
-    private double y;
-    
+    private int x;
+    private int y;
+
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(){
+
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
