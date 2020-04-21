b6ea6c9408a37f9db826f959e7c6a7b2ce63ffb9
Mateusz Wygoda <mateusz.wygoda@gmail.com>
2020-04-21 22:25:41 +0200

    add switch to choose some options

--- src/pl/app/PointApplication.java
+++ src/pl/app/PointApplication.java
@@ -5,16 +5,44 @@ import pl.data.Point;
 
 public class PointApplication {
     public static void main(String[] args) {
+        int choose = 1;
         Point point1 = new Point(4,5);
         PointController control = new PointController();
         System.out.println(point1);
-        control.addX(point1);
-        System.out.println(point1);
-        control.addY(point1);
-        System.out.println(point1);
-        control.minusX(point1);
-        System.out.println(point1);
-        control.minusY(point1);
-        System.out.println(point1);
+        System.out.println("Choose options:");
+        System.out.println("0 - exit");
+        System.out.println("1 - add X");
+        System.out.println("2 - add Y");
+        System.out.println("3 - minus X");
+        System.out.println("4 - minus Y");
+        System.out.println(choose);
+        switch (choose){
+            case 0:
+                System.out.println("Bye bye!");
+                break;
+            case 1:
+                control.addX(point1);
+                System.out.println(point1);
+                break;
+            case 2:
+                control.addY(point1);
+                System.out.println(point1);
+                break;
+            case 3:
+                control.minusX(point1);
+                System.out.println(point1);
+                break;
+            case 4:
+                control.minusY(point1);
+                System.out.println(point1);
+                break;
+            default:
+                System.out.println(point1);
+
+        }
+
+
+
+
     }
 }
